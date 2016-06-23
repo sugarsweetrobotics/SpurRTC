22e316af1da16953288039e3416e1a2096f89e0f
Kenichi Ohara <kohara@meijo-u.ac.jp>
2016-06-23 23:01:09 +0900

    Update FindYPSpur.cmake
    The command of find_path for YPSPUR_LIB_DIR is not correctly semantics.

--- cmake/Modules/FindYPSpur.cmake
+++ cmake/Modules/FindYPSpur.cmake
@@ -5,7 +5,7 @@ endif  ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
 find_path(YPSPUR_INCLUDE_DIR ypspur.h
   HINTS /usr/local/include/ /usr/include/ /opt/local/include)
 
-find_path(YPSPUR_LIB_DIR ypspur
+find_path(YPSPUR_LIB_DIR libypspur.so
   HINTS /usr/local/lib /usr/lib /opt/local/lib)
 
 find_library(YPSPUR_LIBRARY ypspur
